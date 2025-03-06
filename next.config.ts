92138b7fe68541c9f4e92b99cef53353401ea8d6
Ethan Bambock <bamlogick@gmail.com>
2025-03-06 04:56:50 -0800

    testing config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    api: {
-        bodyParser: false,
-    },
 };
 
 export default nextConfig;
