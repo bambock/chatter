081115c7d8ab26ac39330f267e5de89924b24511
Ethan Bambock <bamlogick@gmail.com>
2025-03-08 20:52:22 -0800

    update message formatting

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,25 +1,25 @@
-import { sql } from "@vercel/postgres";
+import { QueryResultRow, sql } from "@vercel/postgres";
 
 export default async function MessagesPage() {
   // Fetch messages from the database
   const { rows } = await sql`SELECT * FROM messages ORDER BY timestamp DESC`;
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div className="m-4">
       <h1>Chat Messages</h1>
       {rows.length === 0 ? (
         <p>No messages found.</p>
       ) : (
-            {rows.map((row) => (
-              <div key={row.id}>
-                <p>
-                  {new Date(row.timestamp).toLocaleString()}
-                </p>
-                <p className="text-red-300">{row.chatid}</p>
-                <p className="text-red-300">{row.sender}</p>
-                <p className="text-red-300">{row.message}</p>
-              </div>
-            ))}
+        <>
+          {rows.map((row: QueryResultRow) => (
+            <div key={row.id}>
+              <p>{new Date(row.timestamp).toLocaleString()}</p>
+              <p className="text-red-300">{row.chatid}</p>
+              <p className="text-red-300">{row.sender}</p>
+              <p className="text-red-300">{row.message}</p>
+            </div>
+          ))}
+        </>
       )}
     </div>
   );
